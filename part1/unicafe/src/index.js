567ccf7e1ba804ec1fdf4b1df92f1a6f158b5007
johan456789 <14802181+johan456789@users.noreply.github.com>
2020-09-02 22:16:06 +0800

    ex1.11

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -23,21 +23,26 @@ const Statistics = ({good, neutral, bad}) => {
   }
 
   return (
-    <div>
+    <table>
       <Stat text='Good' number={good} />
       <Stat text='Good' number={good} />
       <Stat text='Neutral' number={neutral} />
       <Stat text='Bad' number={bad} />
       <Stat text='All' number={good + neutral + bad} />
-      <Stat text='Average' number={(good * 1 + neutral * 0 + bad * -1) / 3} />
-      <Stat text='Positive' number={good / (good + neutral + bad) * 100} unit='%' />
-    </div>
+      <Stat text='Average' number={((good * 1 + neutral * 0 + bad * -1) / 3).toFixed(2)} />
+      <Stat text='Positive' number={(good / (good + neutral + bad) * 100).toFixed(2)} unit='%' />
+    </table>
   )
 }
 
 const Stat = ({text, number, unit}) => {
   return (
-    <p>{text} {number} {unit}</p>
+    <tbody>
+      <tr>
+        <td>{text}</td>
+        <td>{number} {unit}</td>
+      </tr>
+    </tbody>
   )
 }
 
