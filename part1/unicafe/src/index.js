ae9aecbf5fcffc1f025eb5134bbe93a0b2772d76
johan456789 <14802181+johan456789@users.noreply.github.com>
2020-09-02 21:41:06 +0800

    ex1.6

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,6 +1,11 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Header = ({text}) => {
+  return (
+    <h2>{text}</h2>
+  )
+}
 
 const Button = ({handleClick, text}) => {
   return (
@@ -10,11 +15,32 @@ const Button = ({handleClick, text}) => {
   )
 }
 
+const Stats = ({text, count}) => {
+  return (
+    <p>{text} {count}</p>
+  )
+}
+
 const App = (props) => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
-    <div>
-    </div>
+    <>
+      <div>
+        <Header text='Give feedback' />
+        <Button handleClick={() => setGood(good + 1)} text='Good' />
+        <Button handleClick={() => setNeutral(neutral + 1)} text='Neutral' />
+        <Button handleClick={() => setBad(bad + 1)} text='Bad' />
+      </div>
+      <div>
+        <Header text='Statistics' />
+        <Stats text='Good' count={good} />
+        <Stats text='Neutral' count={neutral} />
+        <Stats text='Bad' count={bad} />
+      </div>
+    </>
   )
 }
 
