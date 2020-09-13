b4be8488b2d103c3e8178b3a71f7e47966d30db0
johan456789 <14802181+johan456789@users.noreply.github.com>
2020-09-13 15:24:20 +0800

    ex3.5 & ex3.6

--- part3/phonebook/README.md
+++ part3/phonebook/README.md
@@ -15,3 +15,7 @@
 ## ex3.4
 
 ![src](https://i.imgur.com/v1XSmkz.gif)
+
+## ex3.5 & ex3.6
+
+![src](https://i.imgur.com/96mxx8N.gif)
--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -53,6 +53,26 @@ app.delete('/api/persons/:id', (req, res) => {
   res.status(204).end()
 })
 
+app.use(express.json())  // remember this
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+  if (!body.name || !body.number) {
+    return res.status(400).json({error: 'Name or number missing'})
+  }
+  if (persons.find(p => p.name === body.name)) {
+    return res.status(404).json({error: 'name must be unique'})
+  }
+
+  getRandomInt = (max) => Math.floor(Math.random() * Math.floor(max))
+  const personObject = {
+    "name": body.name,
+    "number": body.number,
+    "id": getRandomInt(1<<10)
+  }
+  persons = persons.concat(personObject)
+  res.json(personObject)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
