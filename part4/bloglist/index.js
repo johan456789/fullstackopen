711fd62f40b2538ee0a9b6f70d41eb352eca9e51
johan456789 <14802181+johan456789@users.noreply.github.com>
2020-09-23 15:21:34 +0800

    add blog model

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -5,15 +5,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = require('./models/blog')
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
