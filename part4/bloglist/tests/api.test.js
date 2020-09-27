8b4e816e811ce1375937a0f788b11d395e9d073f
johan456789 <14802181+johan456789@users.noreply.github.com>
2020-09-28 02:32:28 +0800

    ex4.10

--- part4/bloglist/tests/api.test.js
+++ part4/bloglist/tests/api.test.js
@@ -1,8 +1,27 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Blog = require('../models/blog')
 
 const api = supertest(app)
+
+const initialBlogs = [
+  {
+    title: 'Test',
+    author: 'No name',
+    url: 'https://example.com',
+    likes: 999
+  }
+]
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  for (let blog of initialBlogs) {
+    let blogObject = new Blog(blog)
+    await blogObject.save()
+  }
+})
+
 
 test('getting blog posts', async () => {
   await api
@@ -16,6 +35,34 @@ test('check id property exists', async () => {
   const blogs = response.body
 
   expect(blogs[0].id).toBeDefined()
+})
+
+test('add a new blog post', async () => {
+  // get initial blogs
+  let response = await api.get('/api/blogs')
+  let blogs = response.body
+  const initialLength = blogs.length
+
+  // add new blog post
+  const newBlog = {
+    title: 'Test2',
+    author: 'No name2',
+    url: 'https://example.com2',
+    likes: 1000
+  }
+  response = await api.post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+  const blogID = response.body.id
+
+  // get updated blogs
+  response = await api.get('/api/blogs')
+  blogs = response.body
+  let addedBlog = blogs.filter(x => x.id === blogID)[0]
+  delete addedBlog.id
+  expect(blogs).toHaveLength(initialLength + 1)
+  expect(addedBlog).toEqual(newBlog)
 })
 
 afterAll(() => {
